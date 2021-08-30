8c431dee489c497da4b5a3879417d3a0407d8f25
sgolik <sgolik@unitedthinkers.com>
2021-08-30 12:49:37 +0300

    Added ability to add or update a PET . Closes #56

---
+++ pet-clinic-web/src/main/java/com/sthol/sfgpetclinic/controllers/PetController.java
@@ -0,0 +1,47 @@
+package com.sthol.sfgpetclinic.controllers;
+
+import com.sthol.sfgpetclinic.model.Owner;
+import com.sthol.sfgpetclinic.model.PetType;
+import com.sthol.sfgpetclinic.services.OwnerService;
+import com.sthol.sfgpetclinic.services.PetService;
+import com.sthol.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Controller
+@RequestMapping("owners/{ownerId}")
+public class PetController {
+    private static final String VIEW_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
