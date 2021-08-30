023482f74193f138e73c01dba1f4a18b3c14732c
sgolik <sgolik@unitedthinkers.com>
2021-08-30 17:15:18 +0300

    Implemented Vets List View. Closes #67

--- pet-clinic-data/src/main/java/com/sthol/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/sthol/sfgpetclinic/model/Visit.java
@@ -1,6 +1,7 @@
 package com.sthol.sfgpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "visits")
 public class Visit extends  BaseEntity{
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "date")
     private LocalDate date;
 
